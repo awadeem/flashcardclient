e3265d44420c9eed7c8483578d6b404b892aef3a
cd-alvina <alvina@codedifferently.com>
2022-05-19 19:26:11 -0400

    day 1

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import logo from "./logo.svg";
+import "./App.css";
+import QuestionForm from "./components/quiz/questionForm";
+
+function App() {
+  return <QuestionForm></QuestionForm>;
+}
+
+export default App;
